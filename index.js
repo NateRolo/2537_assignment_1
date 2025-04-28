1a6a4dc732d3e9fc45e1f676089f8533912039f4
Nathan Oloresisimo <146795346+NateRolo@users.noreply.github.com>
2025-04-28 12:38:18 -0700

    Initialize server setup in index.js with required dependencies and configurations

---
+++ index.js
@@ -0,0 +1,12 @@
+'use strict';
+require('dotenv').config();
+const express = require('express');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const bcrypt = require('bcrypt');
+const Joi = require('joi');
+const path = require('path');
+
+const app = express();
+const port = provess.env.PORT || 3000;
+
