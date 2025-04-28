e38ebf81ece95fea5c96d82b2d102e5d20f0a791
Nathan Oloresisimo <146795346+NateRolo@users.noreply.github.com>
2025-04-28 12:46:35 -0700

    feat(config): add environment variable definitions for database and session secrets

--- index.js
+++ index.js
@@ -10,3 +10,10 @@ const path = require('path');
 const app = express();
 const port = provess.env.PORT || 3000;
 
+const mongodb_host = process.env.MONGODB_HOST;
+const mongodb_user = process.env.MONGODB_USER;
+const mongodb_password = process.env.MONGODB_PASSWORD;
+const mongodb_database = process.env.MONGODB_DATABASE;
+const mongodb_session_secret = process.env.MONGODB_SESSION_SECRET;
+const node_session_secret = process.env.NODE_SESSION_SECRET;
+
